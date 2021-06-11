dd9faad36a36e1c1a49a0edb3356389506a2bfff
jzhi <thisxyh@gmail.com>
2021-06-11 22:03:51 +0800

    md -s complete.

--- cmd/markdown.go
+++ cmd/markdown.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
+	"github.com/thisXYH/mdnt/internal"
 	"github.com/thisXYH/mdnt/internal/markdown"
 )
 
@@ -11,9 +16,9 @@ var markdownCmd = &cobra.Command{
 	Short: "维护 markdown 笔记的引用",
 	Long:  "维护 markdown 笔记的引用，以及笔记Id",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// if err := checkAndDealImagesOps(); err != nil {
-		// 	return err
-		// }
+		if err := checkAndDealMarkdownOps(); err != nil {
+			return err
+		}
 
 		// 执行 img 子命令
 		if err := markdown.Execute(*markdownOps); err != nil {
@@ -29,4 +34,25 @@ func init() {
 	markdownCmd.Flags().BoolVarP(&markdownOps.DoIdSet, "id-set", "s", false, "给未设置id的笔记添加id")
 	markdownCmd.Flags().BoolVarP(&markdownOps.DoRelPathFix, "fix-ref", "f", false, "维护笔记的相对路径引用")
 	markdownCmd.Flags().BoolVarP(&markdownOps.NewId, "id-new", "n", false, "生成一个新id")
+}
+
+func checkAndDealMarkdownOps() error {
+	// 两个需要目录的选项
+	if markdownOps.DoIdSet || markdownOps.DoRelPathFix {
+		if markdownOps.MdDir == "" {
+			if markdownOps.MdDir = os.Getenv(mdnt_img_m_env); markdownOps.MdDir == "" {
+				return fmt.Errorf("-m is empty")
+			}
+		}
+
+		if !filepath.IsAbs(markdownOps.MdDir) {
+			markdownOps.MdDir, _ = filepath.Abs(markdownOps.MdDir)
+		}
+
+		if !internal.IsFileOrDirExist(markdownOps.MdDir) {
+			return fmt.Errorf("not found the markdown-dir path: %s", markdownOps.MdDir)
+		}
+	}
+
+	return nil
 }
--- internal/markdown/markdown.go
+++ internal/markdown/markdown.go
@@ -1,16 +1,23 @@
 package markdown
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sync"
 
 	"github.com/thisXYH/mdnt/internal"
 )
 
 type Options struct {
 	MdDir        string //MdDir 文档目录绝对路径
-	DoIdSet      bool   //指定要设置id的文档，或者目录
-	DoRelPathFix bool
-	NewId        bool //生成新Id
+	DoIdSet      bool   //给未设置id的文档设置id
+	DoRelPathFix bool   //维护文档引用
+	NewId        bool   //生成新Id
 }
 
 const IdLen = 32
@@ -21,16 +28,90 @@ func Execute(p Options) error {
 	ops = p
 
 	if p.NewId {
-		fmt.Printf("> id:%s", internal.GenerateHexString(IdLen))
+		fmt.Println(newIdWitFormat())
 	}
 
 	if p.DoIdSet {
-
+		idSet()
 	}
 
 	if p.DoRelPathFix {
-
+		relPathFix()
 	}
 
 	return nil
+}
+
+func idSet() {
+	wg := &sync.WaitGroup{}
+
+	filepath.WalkDir(ops.MdDir, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() || filepath.Ext(d.Name()) != ".md" {
+			return nil
+		}
+		wg.Add(1)
+		go idSetCore(path, d.Name(), wg)
+		return nil
+	})
+
+	wg.Wait()
+}
+
+//idSetCore
+func idSetCore(path, name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	id, moreOneLine := getId(path)
+	if id == "" && moreOneLine {
+		setId(path)
+	}
+}
+
+func setId(path string) {
+	srcR, _ := os.Open(path)
+	srcBr := bufio.NewReader(srcR)
+	tempPath := path + ".temp"
+	tempW, _ := os.Create(tempPath)
+
+	// copy first line.
+	firstLine, _ := srcBr.ReadString('\n')
+	tempW.WriteString(firstLine)
+
+	// write id at second line.
+	tempW.WriteString(newIdWitFormat() + "\r\n")
+
+	// copy other lines.
+	io.Copy(tempW, srcBr)
+	srcR.Close()
+	tempW.Close()
+	os.Rename(tempPath, path)
+}
+
+// getId 获取id，并且标记是否文件大于 1 行。
+func getId(path string) (string, bool) {
+	r, _ := os.Open(path)
+	defer r.Close()
+	rb := bufio.NewReader(r)
+
+	// id在第二行
+	rb.ReadLine()
+	buf, _, err := rb.ReadLine()
+	if err != nil {
+		return "", false
+	}
+
+	reg := regexp.MustCompile(`^> +id:([0-9a-f]{32})`)
+	result := reg.FindAllStringSubmatch(string(buf), -1)
+	if result == nil {
+		return "", true
+	}
+
+	return result[0][1], true
+}
+
+func relPathFix() {
+	// TODO
+}
+
+func newIdWitFormat() string {
+	return fmt.Sprintf("> id:%s", internal.GenerateHexString(IdLen))
 }
